514804fb4cc077362bc401a12fbe4649641632e2
GourdPa <gourdpa@163.com>
2023-07-10 10:56:24 +0800

    自动扫描bean对象注册

--- small-spring-step-13/src/test/java/cn/gourdpa/springframework/test/bean/UserService.java
+++ small-spring-step-13/src/test/java/cn/gourdpa/springframework/test/bean/UserService.java
@@ -4,10 +4,14 @@ import cn.gourdpa.springframework.beans.BeansException;
 import cn.gourdpa.springframework.beans.factory.*;
 import cn.gourdpa.springframework.context.ApplicationContext;
 import cn.gourdpa.springframework.context.ApplicationContextAware;
+import cn.gourdpa.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Component("userService")
 public class UserService implements IUserService {
+
+    private String token;
 
     public String queryUserInfo() {
         try {
@@ -27,4 +31,16 @@ public class UserService implements IUserService {
         return "注册用户：" + userName + " success！";
     }
 
+    @Override
+    public String toString() {
+        return "UserService#token = { " + token + " }";
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
